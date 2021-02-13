cfda9d29b68ff3991e4214418301d134be335c71
alenafedyaeva <291407@gmail.com>
2021-02-13 19:14:24 +0300

    test create del update db

--- homework1/repository/repository_test.go
+++ homework1/repository/repository_test.go
@@ -5,69 +5,183 @@ import (
 	"testing"
 )
 
-func TestCreateItem(t *testing.T){
-	db:=NewMapDB()
 
-	input:=&models.Item{
-		Name: "someName",
+func TestDeleteItem(t *testing.T) {
+	db := NewMapDB()
+
+	input := &models.Item{
+		Name:  "someName",
 		Price: 10,
 	}
-	expected:=&models.Item{
-		ID: 1,
-		Name: input.Name,
+	expected := &models.Item{
+		ID:    1,
+		Name:  input.Name,
 		Price: input.Price,
 	}
-	// 1) Check if item created
-	result,err:= db.CreateItem(input)
-	if err!=nil {
-		t.Error("unexpected err", err)
-	}
-	if expected.Name!= result.Name{
-		t.Errorf("unexpected result: expected %s, result %s", expected.Name,result.Name)
-	}
-	if expected.Price!= result.Price{
-		t.Errorf("unexpected result: expected %d, result %d", expected.Price,result.Price)
-	}
-	if expected.ID!= result.ID{
-		t.Errorf("unexpected result: expected %d, result %d", expected.ID,result.ID)
-	}
-	// 2) Check if item in db ==expected
-	result,err=db.GetItem(expected.ID)
+	// 1) Create item in DB
+	result, err := db.CreateItem(input)
 	if err != nil {
 		t.Error("unexpected err", err)
 	}
-	if expected.Name!= result.Name{
-		t.Errorf("unexpected result: expected %s, result %s", expected.Name,result.Name)
+	if expected.Name != result.Name {
+		t.Errorf("unexpected result: expected %s, result %s", expected.Name, result.Name)
 	}
-	if expected.Price!= result.Price{
-		t.Errorf("unexpected result: expected %d, result %d", expected.Price,result.Price)
+	if expected.Price != result.Price {
+		t.Errorf("unexpected result: expected %d, result %d", expected.Price, result.Price)
 	}
-	if expected.ID!= result.ID{
-		t.Errorf("unexpected result: expected %d, result %d", expected.ID,result.ID)
+	if expected.ID != result.ID {
+		t.Errorf("unexpected result: expected %d, result %d", expected.ID, result.ID)
 	}
-	//3) check increment in db
-	input=&models.Item{
-		Name: "someName2",
-		Price: 19,
-	}
-	expected=&models.Item{
-		ID: 2,
-		Name: input.Name,
-		Price: input.Price,
-	}
-	result,err= db.CreateItem(input)
-	if err!=nil {
+	// 2) Delete existing Item
+	err = db.DeleteItem(result.ID)
+	if err != nil {
 		t.Error("unexpected err", err)
 	}
-	if expected.Name!= result.Name{
-		t.Errorf("unexpected result: expected %s, result %s", expected.Name,result.Name)
+
+	// 3) Try to Get deleted Item from DB
+	result, err = db.GetItem(1)
+	if err != ErrNotFound {
+		t.Error("unexpected result: ", result.ID)
 	}
-	if expected.Price!= result.Price{
-		t.Errorf("unexpected result: expected %d, result %d", expected.Price,result.Price)
+	result, err = db.GetItem(100)
+	if err != ErrNotFound {
+		t.Error("unexpected result: ", result.ID)
+	}
+	//4) Try to delete wrong item
+	// 2) Delete existing Item
+	err = db.DeleteItem(100)
+	if err != ErrNotFound {
+		t.Error("unexpected err", err)
+	}
+}
+
+func TestUpdateItem(t *testing.T) {
+	db := NewMapDB()
+
+	input := &models.Item{
+		Name:  "someName",
+		Price: 10,
+	}
+	expected := &models.Item{
+		ID:    1,
+		Name:  input.Name,
+		Price: input.Price,
+	}
+	// 1) Create item in DB
+	result, err := db.CreateItem(input)
+	if err != nil {
+		t.Error("unexpected err", err)
+	}
+	if expected.Name != result.Name {
+		t.Errorf("unexpected result: expected %s, result %s", expected.Name, result.Name)
+	}
+	if expected.Price != result.Price {
+		t.Errorf("unexpected result: expected %d, result %d", expected.Price, result.Price)
+	}
+	if expected.ID != result.ID {
+		t.Errorf("unexpected result: expected %d, result %d", expected.ID, result.ID)
+	}
+	// 2) Update item
+	input = &models.Item{
+		ID:    1,
+		Name:  "someUpdatetName",
+		Price: 100,
+	}
+	expected = &models.Item{
+		ID:    input.ID,
+		Name:  input.Name,
+		Price: input.Price,
+	}
+	result, err = db.UpdateItem(input)
+	if err != nil {
+		t.Error("unexpected err", err)
+	}
+	if expected.Name != result.Name {
+		t.Errorf("unexpected result: expected %s, result %s", expected.Name, result.Name)
+	}
+	if expected.Price != result.Price {
+		t.Errorf("unexpected result: expected %d, result %d", expected.Price, result.Price)
+	}
+	if expected.ID != result.ID {
+		t.Errorf("unexpected result: expected %d, result %d", expected.ID, result.ID)
+	}
+
+	// 3) Try to update Item with wrong ID
+	input = &models.Item{
+		ID:    100,
+		Name:  "someUpdatetName",
+		Price: 100,
+	}
+	result, err = db.UpdateItem(input)
+	if err != ErrNotFound {
+		t.Error("unexpected err", err)
+	}
+
+}
+
+func TestCreateItem(t *testing.T) {
+	db := NewMapDB()
+
+	input := &models.Item{
+		Name:  "someName",
+		Price: 10,
+	}
+	expected := &models.Item{
+		ID:    1,
+		Name:  input.Name,
+		Price: input.Price,
+	}
+	// 1) Check if item created
+	result, err := db.CreateItem(input)
+	if err != nil {
+		t.Error("unexpected err", err)
+	}
+	if expected.Name != result.Name {
+		t.Errorf("unexpected result: expected %s, result %s", expected.Name, result.Name)
+	}
+	if expected.Price != result.Price {
+		t.Errorf("unexpected result: expected %d, result %d", expected.Price, result.Price)
+	}
+	if expected.ID != result.ID {
+		t.Errorf("unexpected result: expected %d, result %d", expected.ID, result.ID)
+	}
+	// 2) Check if item in db ==expected
+	result, err = db.GetItem(expected.ID)
+	if err != nil {
+		t.Error("unexpected err", err)
+	}
+	if expected.Name != result.Name {
+		t.Errorf("unexpected result: expected %s, result %s", expected.Name, result.Name)
+	}
+	if expected.Price != result.Price {
+		t.Errorf("unexpected result: expected %d, result %d", expected.Price, result.Price)
+	}
+	if expected.ID != result.ID {
+		t.Errorf("unexpected result: expected %d, result %d", expected.ID, result.ID)
+	}
+	//3) check increment in db
+	input = &models.Item{
+		Name:  "someName2",
+		Price: 19,
+	}
+	expected = &models.Item{
+		ID:    2,
+		Name:  input.Name,
+		Price: input.Price,
+	}
+	result, err = db.CreateItem(input)
+	if err != nil {
+		t.Error("unexpected err", err)
+	}
+	if expected.Name != result.Name {
+		t.Errorf("unexpected result: expected %s, result %s", expected.Name, result.Name)
+	}
+	if expected.Price != result.Price {
+		t.Errorf("unexpected result: expected %d, result %d", expected.Price, result.Price)
 	}
 	//check incremented id
-	if expected.ID!= result.ID{
-		t.Errorf("unexpected result: expected %d, result %d", expected.ID,result.ID)
+	if expected.ID != result.ID {
+		t.Errorf("unexpected result: expected %d, result %d", expected.ID, result.ID)
 	}
 
 }
