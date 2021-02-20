265a17acf8afc60e951ae2d19026482e095e7da6
alenafedyaeva <291407@gmail.com>
2021-02-21 01:24:44 +0300

    telegram bot work

--- homework2/main.go
+++ homework2/main.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-	"go-architecture/homework2/service"
+	"flag"
+	"homework2/notification"
 	"homework2/repository"
+	"homework2/service"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-
 func main() {
-	// var tokenStr string
-	// flag.StringVar(&tokenStr, "t", "", "token for telegram api")
-	// flag.Parse()
+	var tokenStr string=""
+	flag.StringVar(&tokenStr, "t", "", "token for telegram api")
+	flag.Parse()
 
-	// tg, err := notification.NewTelegramBot(tokenStr, 323615875)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	tg, err := notification.NewTelegramBot(tokenStr, 1077398714)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	rep := repository.NewMapDB()
 	handler := &server{
-		rep:rep,
-	  service: service.NewService(rep, nil),
+		rep:     rep,
+		service: service.NewService(rep, tg),
 	}
 
-	router:= mux.NewRouter()
+	router := mux.NewRouter()
 	setupServer(router, handler)
-	
-	srv:=&http.Server{
+
+	srv := &http.Server{
 		Addr: ":8085",
 		//Set timeouts to avoid Slowloris attacks
 		// WriteTimeout: time.Second*10,
@@ -42,12 +43,12 @@ func main() {
 }
 
 func setupServer(router *mux.Router, handler *server) {
-	router.HandleFunc("/hello",handler.hello).Methods("GET")
+	router.HandleFunc("/hello", handler.hello).Methods("GET")
 	router.HandleFunc("/items", handler.createItemHandler).Methods("POST")
 	router.HandleFunc("/items", handler.listItemHandler).Methods("GET")
 	router.HandleFunc("/items/{id}", handler.getItemHandler).Methods("GET")
 	router.HandleFunc("/items/{id}", handler.deleteItemHandler).Methods("DELETE")
 	router.HandleFunc("/items/{id}", handler.updateItemHandler).Methods("PUT")
-	router.HandleFunc("/orders",  handler.listOrdersHandler).Methods("GET")
-	router.HandleFunc("/orders",  handler.createOrderHandler).Methods("POST")
+	router.HandleFunc("/orders", handler.listOrdersHandler).Methods("GET")
+	router.HandleFunc("/orders", handler.createOrderHandler).Methods("POST")
 }
--- homework2/service/service.go
+++ homework2/service/service.go
@@ -4,18 +4,18 @@ import (
 	"errors"
 	"homework2/models"
 	"homework2/notification"
-	tg "homework2/pkg/tgbot"
 	"homework2/repository"
 	"log"
 )
+
+
 type Service interface {
-	CreateItem(item *models.Item) (*models.Item, error)
 	CreateOrder(order *models.Order) (*models.Order, error)
 }
 
 type service struct {
-	tg notification.Notification
-	db repository.Repository
+	notif notification.Notification
+	rep   repository.Repository
 }
 
 var (
@@ -24,7 +24,7 @@ var (
 
 func (s *service) CreateOrder(order *models.Order) (*models.Order, error) {
 	for _, itemID := range order.ItemIDs {
-		_, err := s.db.GetItem(itemID)
+		_, err := s.rep.GetItem(itemID)
 		if err != nil && err != repository.ErrNotFound {
 			return nil, err
 		}
@@ -33,32 +33,20 @@ func (s *service) CreateOrder(order *models.Order) (*models.Order, error) {
 		}
 	}
 
-	order, err := s.db.CreateOrder(order)
+	order, err := s.rep.CreateOrder(order)
 	if err != nil {
 		return nil, err
 	}
 
-	if err := s.tg.SendOrderCreated(order); err != nil {
+	if err := s.notif.SendOrderCreated(order); err != nil {
 		log.Println(err)
 	}
 	return order, err
 }
 
-
-func NewService(tg tg.TelegramAPI, db repository.Repository) Service {
+func NewService(rep repository.Repository, notif notification.Notification) Service {
 	return &service{
-		db: db,
-		tg: tg,
+		notif: notif,
+		rep:   rep,
 	}
-}
-
-func (s *service) CreateItem(item *models.Item) (*models.Item, error) {
-	if item.Name == "" {
-		return nil, errors.New("item name is empty")
-	}
-	if item.Price <= 0 {
-		return nil, errors.New("item price should be positive")
-	}
-
-	return s.db.CreateItem(item)
 }
