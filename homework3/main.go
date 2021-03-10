d6e2d28aaf9ebebdec5e4d713c4a8224572c9dfe
alenafedyaeva <291407@gmail.com>
2021-03-11 00:01:41 +0300

    shop

--- homework3/main.go
+++ homework3/main.go
@@ -2,47 +2,61 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"homework2/notification"
+	"homework2/repository"
+	"homework2/service"
 	"log"
 	"net/http"
-	"shop/notification"
-	"shop/repository"
-	"shop/service"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	var tokenStr string
-	flag.StringVar(&tokenStr, "t", "", "token for telegram api")
-
+	var tokenTgStr string 
+	var smtpStrLog,smtpStrPass, smtpReceiver string
+	flag.StringVar(&tokenTgStr, "tg", "", "token for telegram api")
+	flag.StringVar(&smtpStrLog, "smtpL", "", "log for smtp")
+	flag.StringVar(&smtpStrPass, "smtpP", "", "pass for smtp")
+	flag.StringVar(&smtpReceiver,"smtpReceiver","", "receiver smtp")
 	flag.Parse()
 
-	notif, err := notification.NewTelegramBot(tokenStr, 323615875)
+	fmt.Println(tokenTgStr, smtpStrLog, smtpStrPass,smtpReceiver)
+	tg, err := notification.NewTelegramBot(tokenTgStr, 1077398714)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	smtp,err:=notification.NewSMPT(smtpStrLog,smtpStrPass,smtpReceiver)
+
 	rep := repository.NewMapDB()
-	service := service.NewService(rep, notif)
-	s := &server{
-		service: service,
+	handler := &server{
 		rep:     rep,
+		service: service.NewService(rep, tg,smtp),
 	}
 
 	router := mux.NewRouter()
-
-	router.HandleFunc("/items", s.listItemHandler).Methods("GET")
-	router.HandleFunc("/items", s.createItemHandler).Methods("POST")
-	router.HandleFunc("/items/{id}", s.getItemHandler).Methods("GET")
-	router.HandleFunc("/items/{id}", s.deleteItemHandler).Methods("DELETE")
-	router.HandleFunc("/items/{id}", s.updateItemHandler).Methods("PUT")
-
-	router.HandleFunc("/orders", s.listOrdersHandler).Methods("GET")
-	router.HandleFunc("/orders", s.createOrderHandler).Methods("POST")
+	setupServer(router, handler)
 
 	srv := &http.Server{
-		Addr:    ":8081",
+		Addr: ":8085",
+		//Set timeouts to avoid Slowloris attacks
+		// WriteTimeout: time.Second*10,
+		// ReadTimeout: time.Second*15,
+		// IdleTimeout: time.Second*60,
 		Handler: router,
 	}
+
 	log.Fatal(srv.ListenAndServe())
+}
+
+func setupServer(router *mux.Router, handler *server) {
+	router.HandleFunc("/hello", handler.hello).Methods("GET")
+	router.HandleFunc("/items", handler.createItemHandler).Methods("POST")
+	router.HandleFunc("/items", handler.listItemHandler).Methods("GET")
+	router.HandleFunc("/items/{id}", handler.getItemHandler).Methods("GET")
+	router.HandleFunc("/items/{id}", handler.deleteItemHandler).Methods("DELETE")
+	router.HandleFunc("/items/{id}", handler.updateItemHandler).Methods("PUT")
+	router.HandleFunc("/orders", handler.listOrdersHandler).Methods("GET")
+	router.HandleFunc("/orders", handler.createOrderHandler).Methods("POST")
 }
